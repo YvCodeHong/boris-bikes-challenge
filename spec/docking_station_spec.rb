9da069c8eee1c560d771ec93ecbd53f1100089c3
Elishka Flint <elishkaf@gmail.com>
2018-06-12 16:35:52 +0100

    Wrote and tested release bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,18 @@
 require "docking_station"
 
 describe DockingStation do
-
+ it { is_expected.to respond_to :release_bike}
 end
+
+
+
+
+
+
+=begin
+describe DockingStation do
+ it 'should respond to the release_bike method' do
+	 expect(subject).to respond_to :release_bike
+ end
+end
+=end
