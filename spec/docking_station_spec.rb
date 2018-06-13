7bc2d74067a036c64c20c9511c3cf2169ff2e585
Daniel Lau <daniellau@Daniels-MacBook-Pro-2.local>
2018-06-13 17:23:16 +0100

    Added ability to dock bike and tested

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,15 +2,27 @@ require "docking_station"
 
 describe DockingStation do
 
- it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to(:release_bike)}
 
- it "releases a working bike" do
-   bike = subject.release_bike
-   expect(bike).to be_working
- end
+  it "releases a working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 
   it { is_expected.to respond_to(:dock).with(1).argument}
 
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
 end
 
