add306089c8d66760f5ee52e4bc396738cc5c695
Yvonne Tang <ytang604@gmail.com>
2018-06-15 17:03:59 +0100

    challenge13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,13 +3,16 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
-  def release_bike
-    Bike.new
-  end
-
   def dock(bike)
+    fail 'No spaces available' if @bike
     @bike = bike
   end
 
-  
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+
+
+
 end
